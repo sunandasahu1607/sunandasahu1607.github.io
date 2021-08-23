e9dded5540ac3517dfc4413be7d99dae284ee34a
sunandasahu1607 <89378582+sunandasahu1607@users.noreply.github.com>
2021-08-23 15:33:45 +0530

    sheet exp

---
+++ js/sheet.js
@@ -0,0 +1,34 @@
+
+
+    var api_key='AIzaSyCbUYf0QIevZMDVR-eOkkFOBfvjars6DVY';
+    var sheet_id='1E4Srt_E8PzTv1t8UwXc10JLviEzTjCi9qttqMQ2e1Fo';
+    var tab_name='All_Values';
+   
+    var contentRepoURL='https://sheets.googleapis.com/v4/spreadsheets/'
+    +sheet_id+'/values/'
+    +tab_name+'?alt=json&key='
+    +api_key;
+
+    fetch(contentRepoURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+       // appendData(data);
+       console.log(data)
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+
+    
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+        mainContainer.appendChild(div);
+    }
+}
+    
